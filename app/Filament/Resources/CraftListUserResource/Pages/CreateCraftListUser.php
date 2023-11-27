137c3e8495bc05d07bbf5b99ad3dc3459a8703e7
夏建荣 <xiajianrong@mabangerp.com>
2023-11-27 17:27:00 +0800

    fix

---
+++ app/Filament/Resources/CraftListUserResource/Pages/CreateCraftListUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CraftListUserResource\Pages;
+
+use App\Filament\Resources\CraftListUserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCraftListUser extends CreateRecord
+{
+    protected static string $resource = CraftListUserResource::class;
+}
